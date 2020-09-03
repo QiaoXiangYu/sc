07d19261a4211c1159b7a209c81ecc4b099bd440
QiaoXiangYu <1163923622@qq.com>
2020-09-03 16:15:26 +0800

    自动化构建打包

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './index.less';
+// 引入 iconfont 样式文件
+import './iconfont.css';
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(9, 2));
+console.log(add(2, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -10,36 +10,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.less$/,
-        // 要使用多个loader处理用use
-        use: ['style-loader', 'css-loader', 'less-loader']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
+      // 打包其他资源(除了html/js/css资源以外的资源)
       {
-        // 问题：默认处理不了html中img图片
-        // 处理图片资源
-        test: /\.(jpg|png|gif)$/,
-        // 使用一个loader
-        // 下载 url-loader file-loader
-        loader: 'url-loader',
+        // 排除css/js/html资源
+        exclude: /\.(css|js|html|less)$/,
+        loader: 'file-loader',
         options: {
-          // 图片大小小于8kb，就会被base64处理
-          // 优点: 减少请求数量（减轻服务器压力）
-          // 缺点：图片体积会更大（文件请求速度更慢）
-          limit: 8 * 1024,
-          // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是commonjs
-          // 解析时会出问题：[object Module]
-          // 解决：关闭url-loader的es6模块化，使用commonjs解析
-          esModule: false,
-          // 给图片进行重命名
-          // [hash:10]取图片的hash的前10位
-          // [ext]取文件原来扩展名
           name: '[hash:10].[ext]'
         }
-      },
-      {
-        test: /\.html$/,
-        // 处理html文件的img图片（负责引入img，从而能被url-loader进行处理）
-        loader: 'html-loader'
       }
     ]
   },
@@ -48,5 +29,19 @@ module.exports = {
       template: './src/index.html'
     })
   ],
-  mode: 'development'
+  mode: 'development',
+
+  // 开发服务器 devServer：用来自动化（自动编译，自动打开浏览器，自动刷新浏览器~~）
+  // 特点：只会在内存中编译打包，不会有任何输出
+  // 启动devServer指令为：npx webpack-dev-server
+  devServer: {
+    // 项目构建后路径
+    contentBase: resolve(__dirname, 'build'),
+    // 启动gzip压缩
+    compress: true,
+    // 端口号
+    port: 3000,
+    // 自动打开浏览器
+    open: true
+  }
 };
