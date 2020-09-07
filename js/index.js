baaf634bc1aba1e902cf3140a77f49b4efcf5d12
QiaoXiangYu <1163923622@qq.com>
2020-09-07 16:27:38 +0800

    打包压缩css样式

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 //引入css资源
-import '../css/index.css';
-import '../css/index.less';
+import '../css/a.css';
+import '../css/b.css';
 
-function f1(a,b) {
-    return a+b;
-}
-console.log(f1(4,7));
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,47 @@
-/*
-  开发环境配置：能让代码运行
-    运行项目指令：
-      webpack 会将打包结果输出出去
-      npx webpack-dev-server 只会在内存中编译打包，没有输出
-*/
-
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+// 设置nodejs环境变量
+// process.env.NODE_ENV = 'development';
 
 module.exports = {
-  entry: './js/index.js',
+  entry: './src/js/index.js',
   output: {
-    filename: 'built.js',
+    filename: 'js/built.js',
     path: resolve(__dirname, 'build')
   },
   module: {
     rules: [
-      // loader的配置
       {
-        // 处理css资源
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        // 处理less资源
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      },
-      {
-        // 处理图片资源
-        test: /\.(jpg|png|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8 * 1024,
-          name: '[hash:10].[ext]',
-          // 关闭es6模块化
-          esModule: false,
-          outputPath: 'imgs'
-        }
-      },
-      {
-        // 处理html中img资源
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        // 处理其他资源
-        exclude: /\.(html|js|css|less|jpg|png|gif)/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash:10].[ext]',
-          outputPath: 'media'
-        }
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                // postcss的插件
+                require('postcss-preset-env')()
+              ]
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
-    // plugins的配置
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css'
+    }),
+    // 压缩css
+    new OptimizeCssAssetsWebpackPlugin()
   ],
-  mode: 'development',
-  devServer: {
-    contentBase: resolve(__dirname, 'build'),
-    compress: true,
-    port: 3000,
-    open: true
-  }
+  mode: 'development'
 };
