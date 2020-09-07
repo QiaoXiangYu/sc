aa0e5d320a4c4db3b65601abdd72966e14abe898
QiaoXiangYu <1163923622@qq.com>
2020-09-07 20:57:14 +0800

    打包压缩html文件

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-import '../css/a.css';
-import '../css/b.css';
+// import '@babel/polyfill';
+
+const add = (x, y) => {
+  return x + y;
+};
+console.log(add(2, 5));
+
+const promise = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('定时器执行完了~');
+    resolve();
+  }, 1000);
+});
+
+console.log(promise);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
-
-// 设置nodejs环境变量
-// process.env.NODE_ENV = 'development';
 
 module.exports = {
   entry: './src/js/index.js',
@@ -12,36 +7,17 @@ module.exports = {
     filename: 'js/built.js',
     path: resolve(__dirname, 'build')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                // postcss的插件
-                require('postcss-preset-env')()
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/built.css'
-    }),
-    // 压缩css
-    new OptimizeCssAssetsWebpackPlugin()
+      template: './src/index.html',
+      // 压缩html代码
+      minify: {
+        // 移除空格
+        collapseWhitespace: true,
+        // 移除注释
+        removeComments: true
+      }
+    })
   ],
-  mode: 'development'
+  mode: 'production'
 };
